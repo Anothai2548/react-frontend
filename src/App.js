7c74388587be96b6bfd709b68e7c4c76b9494df5
Anothai <anothai.soon@northbkk.ac.th>
2023-09-19 12:41:29 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Routes, Route, Link } from 'react-router-dom' 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import AddTutorial from './components/add-tutorial.js'
+import Tutorial from './components/tutorial.js'
+import TutorialsList from './components/tutorials-list.js'
+import About from './components/about.js'
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <nav className='navbar navbar-expand navbar-secondary bg-secondary'>
+          <Link to='/' className='navbar-brand'>
+            NBU
+          </Link>
+          <div className='navbar-nav mr-auto'>
+            <li className='nav-item'>
+              <Link to='/tutorials' className='nav-link'> 
+                Lessons              
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/add' className='nav-link'> 
+                Add              
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/about' className='nav-link'> 
+                About              
+              </Link>
+            </li>
+          </div>
+        </nav>
+
+        <div className='continer mt-3'>
+          <Routes>
+            <Route path='/' element={<TutorialsList />} />
+            <Route path='/tutorials' element={<TutorialsList />} />
+            <Route path='/add' element={<AddTutorial />} />
+            <Route path='/tutorial/:id' element={<Tutorial />} />
+            <Route path='/about' element={<About />} />
+          </Routes>
+        </div>
+      </>
+    )
+  }
 }
 
-export default App;
+export default App
